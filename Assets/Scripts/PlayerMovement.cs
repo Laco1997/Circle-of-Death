e0470989c20a7c05d9ca4aedfe063f67b4b01d05
Laco1997 <lelkesl1997@gmail.com>
2022-10-01 00:04:41 +0200

    Experimenting with player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 8f;
+
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool onGround;
+
+    float horizontal;
+    float vertical;
+    Vector3 move;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ground check
+        onGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (onGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+
+        PlayerInput();
+
+        MovePlayer();
+
+    }
+
+    private void PlayerInput()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        move = transform.right * horizontal + transform.forward * vertical;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
