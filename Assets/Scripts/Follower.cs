ce05026cf535d713abd845ffd488efcd59f56983
Matej Janeček <matej.janecek19@gmail.com>
2022-10-30 17:28:57 +0100

    stage 1 finish

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -19,9 +21,17 @@ public class Follower : MonoBehaviour
     public float beamCooldownDefault = 10f;
     private float beamCooldown;
     public float beamCastTime = 5f;
+    public float beamDuration = 5f;
     private float beamCast = -1;
     private Vector3 beamTargetPosition = Vector3.zero;
     public GameObject beamParticles;
+    public GameObject boss_pole;
+
+    public GameObject[] poles;
+    private bool pickupPolePhase = false;
+    private GameObject lastPole = null;
+
+
 
     public int stage = 1;
 
@@ -32,17 +42,32 @@ public class Follower : MonoBehaviour
         playerTransform = GetComponent<Transform>();
         this.beamCooldown = this.beamCooldownDefault;
         //this.beamParticles = GameObject.FindWithTag("Beam");
+        poles = GameObject.FindGameObjectsWithTag("Pole");
     }
 
     void stage1()
     {
-        if (this.beamPhase)
+        if (this.pickupPolePhase)
         {
+
+            if (npc.remainingDistance < 5f) // todo fix
+            {
+                // todo dust particles
+                //GameObject.FindGameObjectWithTag("BossPole")
+                    boss_pole.SetActive(true);
+                this.lastPole.SetActive(false);
+                this.stage = 2;
+            }
+            else
+            {
+                npc.SetDestination(this.lastPole.transform.position);
+            }
+        }else if (this.beamPhase){
             if (this.inMiddle)
             {
+                npc.transform.LookAt(player.transform);
                 if (this.beamTargetPosition == Vector3.zero)
                 {
-                    npc.transform.LookAt(player.transform);
                     // cast beam
                     if (this.beamCast == -1)
                     {
@@ -54,7 +79,6 @@ public class Follower : MonoBehaviour
                         if (this.beamCast >= this.beamCastTime)
                         {
                             this.beamTargetPosition = new Vector3(player.transform.position.x, 1.9f, player.transform.position.z);
-                            
                         }
                     }
                 }
@@ -63,6 +87,44 @@ public class Follower : MonoBehaviour
                     // beam casting animation
                     //Debug.DrawLine(new Vector3(transform.position.x, 5, transform.position.z), this.beamTargetPosition, Color.red);
                     this.beamParticles.SetActive(true);
+                    this.beamCast += Time.deltaTime;
+                    if (this.beamCast - this.beamCastTime >= this.beamDuration)
+                    {
+                        /*
+                        if (Physics.Linecast(this.transform.position, player.position, out RaycastHit hitInfo))
+                        {
+                            Debug.Log("blocked");
+                            Debug.Log(hitInfo);
+                            hitInfo.collider.gameObject.SetActive(false);
+                        }
+                        */
+                        int max = -1;
+                        GameObject poleWithMax = null;
+                        foreach (GameObject pole in this.poles.Where(p => p.activeSelf))
+                        {
+                            int hits = pole.GetComponent<PoleHit>().hits;
+                            if (hits > 0 && hits > max) // todo if hits > const
+                            {
+                                max = hits;
+                                poleWithMax = pole;
+                            }
+                        }
+                        if (poleWithMax)
+                        {
+                            poleWithMax.SetActive(false);
+                        }
+                        this.beamParticles.SetActive(false);
+                            // stop cast
+                        // destroy pole if targetted
+
+                        //continue normally
+                        beamPhase = false;
+                        goingToMiddle = false;
+                        inMiddle = false;
+                        beamCooldown = beamCooldownDefault;
+                        beamCast = 0;
+                        beamTargetPosition = Vector3.zero;
+}
                 }
             }
             else
@@ -75,14 +137,14 @@ public class Follower : MonoBehaviour
                         npc.transform.position = new Vector3(middlePoint.position.x, npc.transform.position.y, middlePoint.position.z);
                         this.inMiddle = true;
                         animator.SetBool("isFollowing", false);
-                        Debug.Log("inMiddle = true");
+                        //Debug.Log("inMiddle = true");
                     }
-                    Debug.Log(npc.remainingDistance);
+                    //Debug.Log(npc.remainingDistance);
                 }
                 else
                 {
                     npc.SetDestination(middlePoint.position);
-                    Debug.Log("goingToMiddle = true");
+                    //Debug.Log("goingToMiddle = true");
                     this.goingToMiddle = true;
                 }
             }
@@ -103,18 +165,33 @@ public class Follower : MonoBehaviour
                 }
                 else if (this.beamCooldown <= 0)
                 {
-                    this.beamPhase = true;
-                    animator.SetBool("isFollowing", true);
-                    Debug.Log("Going to middle");
+                    if (this.poles.Where(p => p.activeSelf).ToList().Count == 1)
+                    {
+                        this.pickupPolePhase = true;
+                    }
+                    else
+                    {
+                        this.beamPhase = true;
+                        animator.SetBool("isFollowing", true);
+                        //Debug.Log("Going to middle");
+                    }
                 }
             }
             else
             {
                 if (this.beamCooldown <= 0)
                 {
-                    this.beamPhase = true;
-                    animator.SetBool("isFollowing", true);
-                    Debug.Log("Going to middle");
+                    if (this.poles.Where(p => p.activeSelf).ToList().Count == 1)
+                    {
+                        this.pickupPolePhase = true;
+                        this.lastPole = this.poles.Where(p => p.activeSelf).First();
+                    }
+                    else
+                    {
+                        this.beamPhase = true;
+                        animator.SetBool("isFollowing", true);
+                        //Debug.Log("Going to middle");
+                    }
                 }
                 else
                 {
@@ -146,12 +223,21 @@ public class Follower : MonoBehaviour
 
     }
 
+    void stage2()
+    {
+        npc.SetDestination(player.position);
+        Debug.Log("Stage2");
+    }
+
     void Update()
     {
         switch (this.stage)
         {
             case 1:
                 this.stage1();
+                break;
+            case 2:
+                this.stage2();
                 break;
         }
     }
