aed284241ab74fd380ff10756e0902fb935b3d4a
Laco1997 <lelkesl1997@gmail.com>
2022-12-05 20:11:18 +0100

    Fix post merge

--- Assets/Scripts/MeleeBossHit.cs
+++ Assets/Scripts/MeleeBossHit.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public class MeleeBossHit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("HIT");
