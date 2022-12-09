81787d5bf5869b931441992d8bf1ed9c954fce8b
Laco1997 <lelkesl1997@gmail.com>
2022-12-09 14:09:53 +0100

    Fix build issues

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditor.PackageManager;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
